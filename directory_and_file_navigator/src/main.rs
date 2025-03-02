2a720cec00f55912d9302e302a190b849338b68b
Karim ABOUSSELHAM <karim.abousselham@gmail.com>
2025-03-02 12:38:57 +0000

    Answer to question 4 after reducing the number of 'if let' statments from 9 to 1

--- directory_and_file_navigator/src/main.rs
+++ directory_and_file_navigator/src/main.rs
@@ -1,6 +1,6 @@
 use std::{
     env::current_dir,
-    fs::{read_dir,read_to_string},
+    fs::{read_dir,read_to_string, write, /*Metadata,*/ DirEntry},
     path::PathBuf,
     };
 use lazy_static::lazy_static;
@@ -58,10 +58,38 @@ impl DirectoryApp {
         }
     }
     fn save_file(&mut self) {
-
-        if let Err(e) = std::fs::write(&self.file_loc, &self.edited_content) {
+        // if let expression was refactored here
+        write(&self.file_loc, &self.edited_content)
+        .map(|_| self.is_saved=true)
+        .unwrap_or_else(|e| {eprintln!("Error saving file: {}", e)})
+        /*if let Err(e) = write(&self.file_loc, &self.edited_content) {
             eprintln!("Error saving file: {}", e);
-        } else {self.is_saved=true;}
+        } else {self.is_saved=true;}*/
+    }
+    //new method to refactor how we handle each entry when reading the current directory
+    fn process_entry(&mut self, entry: DirEntry, ui: &mut egui::Ui) {
+        let metadata=  match entry.metadata() {
+            Ok(meta) => meta,
+            Err(_)=> return
+        };
+        let name= match entry.file_name().into_string() {
+            Ok(n) => n,
+            Err(_) => return,
+        };
+        match metadata.file_type() {
+            t if t.is_dir() => {if ui.button(RichText::new(&name).color(Color32::GRAY)).clicked(){
+                self.current_dir.push(&name);
+                }
+            },
+            t if t.is_file() => {if ui.button(RichText::new(&name).color(Color32::GOLD))
+                .clicked(){
+                    let new_file_loc: PathBuf = self.current_dir.join(&name);
+                    // Check if there are unsaved changes before switching files
+                    self.load_file(new_file_loc);
+                }
+            },
+            _ => {ui.label(format!("{:?}", metadata.file_type()));},
+        }
     }
 
 }
@@ -73,52 +101,26 @@ lazy_static! {
 
 impl eframe::App for DirectoryApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-            egui::CentralPanel::default().show(ctx, |ui| {
+        egui::CentralPanel::default().show(ctx, |ui| {
                 // Add space to prevent overlap with the top panel which will include the checkbox
-                ui.add_space(25.0);
-                egui::ScrollArea::vertical().show(ui, |ui| {
+            ui.add_space(25.0);
+            egui::ScrollArea::vertical().show(ui, |ui| {
                 //This part is pretty easy! Make a button and .pop() when it is clicked.
                 if ui.button(" .. ").clicked() {
                     self.current_dir.pop();
                 }
                 //Now we are going to work through the directory information.
                 //The unwraps have been removed, but egui’s update() method doesn’t return a Result, so we can’t use the
-                //question mark operator. The if let syntax is helpful here.
+                //question mark operator. 
                 if let Ok(read_dir) = read_dir(&self.current_dir) {
                 //Note that here we are using .flatten() to ignore anything inside the read_dir() 
                 //method that returns an Err.
                     for entry in read_dir.flatten() {
                         //We get the metadata and file/directory name. With the metadata we can see
                         //whether we have a file or a directory.
-                        if let Ok(metadata) = entry.metadata(){
-                            if metadata.is_dir() {
-                                if let Ok(dir_name) = entry.file_name().into_string(){
-                        //We’ll make buttons with different text depending on whether we have a file or a
-                        //directory. If we have a directory, clicking the button will .push() to the
-                        //PathBuf and move us into that directory.
-                                    if ui
-                                    .button(RichText::new(&dir_name).color(Color32::GRAY))
-                                    .clicked(){
-                                        self.current_dir.push(&dir_name);
-                                    }
-                                }
-                        } else if metadata.is_file() {
-                            if let Ok(file_name)=entry.file_name().into_string() {
-                                if ui
-                                .button(RichText::new(&file_name).color(Color32::GOLD))
-                                .clicked()
-                                {
-                                    let new_file_loc: PathBuf = self.current_dir.join(&file_name);
-                                    // Check if there are unsaved changes before switching files
-                                    self.load_file(new_file_loc);
-                                }
-                            }
-                        } else {
-                            ui.label(format!("{:?}", metadata.file_type()));
-                            }
+                        self.process_entry(entry, ui);
                         }
                     }
-                }
             });
         });
         if self.show_unsaved {
@@ -129,55 +131,64 @@ impl eframe::App for DirectoryApp {
                 .show(ctx, |ui| {
                     if ui.button("Save Changes").clicked() {
                         // Save the file content and load the next file
-                        if let Err(e) = std::fs::write(&self.file_loc, &self.edited_content) {
+                        //Refactoring of "if let" expression made here
+                        write(&self.file_loc, &self.edited_content)
+                        .map(|_| {self.is_saved=true})
+                        .unwrap_or_else(|e| {eprintln!("Error saving file: {}", e)});
+                        /*if let Err(e) = std::fs::write(&self.file_loc, &self.edited_content) {
                             eprintln!("Error saving file: {}", e);
                         } else {
                             self.is_saved = true;
-                        }
+                        }*/
                         self.show_unsaved = false;
-                        if let Some(new_file_loc) = self.pending_file_loc.take() {
+                        //Refactoring of "if let" expression made here
+                        self.pending_file_loc.take()
+                        .map(|new_file_loc| {self.update_file_content(new_file_loc)});
+                        /*if let Some(new_file_loc) = self.pending_file_loc.take() {
                             self.update_file_content(new_file_loc);
-                        }
+                        }*/
                     }
                     if ui.button("Discard Changes").clicked() {
                         self.show_unsaved = false; // Close the unsaved prompt
-                        // Discard changes and load the content of the next file
-                        if let Some(new_file_loc) = self.pending_file_loc.take() {
+                        // Discard changes and load the content of the next file while refactoring the "if let" expression
+                        self.pending_file_loc.take()
+                        .map(|new_file_loc| {self.update_file_content(new_file_loc)});
+                        /*if let Some(new_file_loc) = self.pending_file_loc.take() {
                             self.update_file_content(new_file_loc);
-                        }
+                        }*/
                         
                     }
-                });
+            });
         }
 
         egui::TopBottomPanel::top("top_panel").show(ctx, |ui| { 
             let mut is_checked = IS_CHECKED.lock().unwrap();
             ui.horizontal(|ui| {
-            ui.add(Checkbox::new(&mut is_checked, "Show file content")); 
-            if *is_checked && !self.file_content.is_empty() {
-                if ui.button("Save").clicked() {
-                        self.save_file();
-                }
+                ui.add(Checkbox::new(&mut is_checked, "Show file content")); 
+                if *is_checked && !self.file_content.is_empty() {
+                    if ui.button("Save").clicked() {
+                            self.save_file();
+                    }
                 }
             });
         });    
-            if *IS_CHECKED.lock().unwrap() {
-                //If the checkbox is checked, we display the file content in a new panel on the right side.
-                let width = ctx.screen_rect().max.x / 2.0;
-                if !self.file_content.is_empty() {
-                    egui::SidePanel::right("Text viewer")
-                    .min_width(width)
-                    .show(ctx, |ui| {
-                        ui.add_space(25.0);
-                        let response = ui.add(TextEdit::multiline(&mut self.edited_content)
-                        .desired_width(width));
-                        // Detect changes and update is_saved
-                        if response.changed() {
-                            self.is_saved = false;
-                        }
-                    });
-                }
+        if *IS_CHECKED.lock().unwrap() {
+            //If the checkbox is checked, we display the file content in a new panel on the right side.
+            let width = ctx.screen_rect().max.x / 2.0;
+            if !self.file_content.is_empty() {
+                egui::SidePanel::right("Text viewer")
+                .min_width(width)
+                .show(ctx, |ui| {
+                    ui.add_space(25.0);
+                    let response = ui.add(TextEdit::multiline(&mut self.edited_content)
+                    .desired_width(width));
+                    // Detect changes and update is_saved
+                    if response.changed() {
+                        self.is_saved = false;
+                    }
+                });
             }
+        }
     }
 }
 
